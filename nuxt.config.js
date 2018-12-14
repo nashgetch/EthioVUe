e54307ac8c1d8f78945b194bdd62be8336bec72c
nashgetch <nashgetch@12gmail.com>
2018-12-14 16:53:11 +0300

    live channels

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ module.exports = {
         charset: 'utf-8'
       }
     ],
-    // __dangerouslyDisableSanitizers: ['script'],
+    __dangerouslyDisableSanitizers: ['script'],
     link: [{
         rel: 'icon',
         type: 'image/x-icon',
