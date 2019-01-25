48e399111b65417700e0659e6fa25475b3a5dceb
nashgetch <nashgetch@12gmail.com>
2019-01-25 11:05:15 +0300

    api

--- components/single_channel/live.vue
+++ components/single_channel/live.vue
@@ -17,7 +17,7 @@
           <h1 :class="marginTop(channel.owner_id)" class="display-4" style="font-size:1rem">
             <a>{{channel.tv_name}}</a>
           </h1>
-           <div class="nashians" v-if="videos.length < 4">
+           <div class="nashian" v-if="videos.length < 4">
               <adsbygoogle :ad-slot="'5950915078'"/>
             </div>
           <!-- Ads block -->
