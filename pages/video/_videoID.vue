1d370e25ebb17d272b457a4511f92f3e8d0b9cef
nashgetch <nashgetch@12gmail.com>
2019-02-01 16:16:15 +0300

    righteous

--- pages/video/_videoID.vue
+++ pages/video/_videoID.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="nashian">
-    <adsbygoogle :ad-slot="'5950915078'"/>
+  <div class="container-fluid top-margin-bn-sm-md" style="padding-top: .2rem;">
+    <div class="nashian">
+      <adsbygoogle :ad-slot="'5950915078'"/>
+    </div>
   </div>
 </template>
