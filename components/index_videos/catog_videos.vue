83bac3fa16da5038924436427e4e8647af7d0d04
nashgetch <nashgetch@12gmail.com>
2019-02-02 12:41:49 +0300

    the life of david gale

--- components/index_videos/catog_videos.vue
+++ components/index_videos/catog_videos.vue
@@ -69,9 +69,7 @@
               </div>
             </div>
           </div>
-          <div style="margin-left: auto; margin-right: auto;" class="nashians">
-            <adsbygoogle :ad-slot="'5950915078'"/>
-          </div>
+
         </div>
       </div>
       <!-- </div> -->
--- components/index_videos/trend_videos.vue
+++ components/index_videos/trend_videos.vue
@@ -62,9 +62,7 @@
               <!-- </div> -->
             </div>
           </div>
-          <div style="margin-left: auto; margin-right: auto;" class="nashians">
-            <adsbygoogle :ad-slot="'5950915078'"/>
-          </div>
+
         </div>
       </div>
       <!-- </div> -->
