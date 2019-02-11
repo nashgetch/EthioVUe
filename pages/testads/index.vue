1f18bfd0cd3e4952744c1c6bc6fb9e62627dada6
nashgetch <nashgetch@12gmail.com>
2019-02-11 13:35:25 +0300

    seo

--- pages/testads/index.vue
+++ pages/testads/index.vue
@@ -255,15 +255,134 @@
         </div>
       </div>
     </div>
-    <LatestVod :videos="videos" v-if="checkTab(1)"/>
-    <CatogVods v-if="checkTab(2)"/>
-    <TrendingVods v-if="checkTab(3)"/>
-    <Subscription v-if="checkTab(4)"/>
+    <!-- <LatestVod :videos="videos" v-if="checkTab(1)"/> -->
+     <div class="container parentDiv">
+      <!-- <div class="nashians">
+        <adsbygoogle/>
+      </div>-->
+
+      <span class="title">Other Recommended Videos...</span>
+
+      <hr class="guetta">
+      <div class="row display-flex">
+        <!-- <div class="col-12 col-sm-6 col-md-4 col-lg-3 ml-auto mr-auto mb-3">
+          <div class="kaleb-vids">sa
+            <div class="Vimg" style="background-color:#fbe631 !important;">
+              <div class="row chn-image-container index m-0">
+                <div class="col-6">
+                  <a href="/live-channels">
+                    <div class="small-logo mt-3 index">
+                      <img src="/img/fbctv_poster.jpg" alt="Fana">
+                    </div>
+                  </a>
+                </div>
+                <div class="col-6">
+                  <a href="/live-channels">
+                    <div class="small-logo mt-3 index">
+                      <img src="/img/amma_poster.jpg" alt="Amhara">
+                    </div>
+                  </a>
+                </div>
+                <div class="col-6">
+                  <a href="/live-channels">
+                    <div class="small-logo mt-3 index">
+                      <img src="/img/walta_posters.jpg" alt="Walta">
+                    </div>
+                  </a>
+                </div>
+                <div class="col-6">
+                  <a href="/live-channels">
+                    <div class="small-logo mt-3 index">
+                      <img src="/img/obm_poster.jpg" alt="OBN">
+                    </div>
+                  </a>
+                </div>
+              </div>
+              <div class="descr p-0 mb-2">
+                <h1 style="font-size: 14px !important;">
+                  <a class="hideOverflow">Our Live Channels</a>
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>-->
+        <div
+          class="col-12 col-sm-6 col-md-3 col-lg-2 videoitem kygo"
+          v-for="(video,$index) in videos"
+          :key="$index"
+        >
+          <div class="kaleb-vids">
+            <div class="Vimg itemContainer" style="background-color: black;">
+              <a :href="'/single-video/'+video.v_id">
+                <!-- <clazy-load :src="'//video2.vixtream.net/'+video.filename">
+                  <div
+                        slot="placeholder"
+                        class="bg-inverse"
+                        style="background-color: black; height:145px;"
+                      >
+                You can put any component you want in here.-->
+                <!-- </div> -->
+                <img :src="'//video2.vixtream.net/'+video.filename" :alt="video.title"/>
+                <div ng-click="viewVideo(video.v_id)" class="play">
+                  <i class="fa fa-play-circle-o playbtn" style="font-size:48px"></i>
+                </div>
+                <!-- </clazy-load> -->
+              </a>
+              <div class="time">{{video.duration}}</div>
+              <div
+                ng-style="hiddenPlus"
+                v-if="loggedIn"
+                class="nashh"
+                @click="add_to_watchlist(user.id, video.v_id)"
+              >
+                <i class="fa fa-plus"></i>
+              </div>
+              <div class="nashhh">{{video.type.toUpperCase()}}</div>
+            </div>
+            <div class="descr main" ng-click="viewVideo(video.v_id)">
+              <h1 style="font-size: 14px !important;">
+                <a class="text" :aria-label="video.title" :title="video.title">{{video.title}}</a>
+                <a
+                  class="text smallFont"
+                  :href="'/single-video/' + video.v_id"
+                  :aria-label="video.title_en"
+                  :title="video.title_en"
+                >{{video.title_en}}</a>
+              </h1>
+              <!-- <h1 v-show="engshow(video.v_id)" style="font-size: 14px !important;"><a
+                  class="hideOverflow"
+                  :aria-label="video.title"
+                  :title="video.title_en"
+              >{{video.title_en}}</a></h1>-->
+            </div>
+            <!-- <div class="views calvin"> -->
+            <ViewCatogs
+              :vid="video.v_id"
+              :cat_id="video.category_id"
+              :isIndex="true"
+              :title_en="video.title_en"
+              :created_at="video.created_at"
+              :view_count="video.view_count"
+              :type="video.type"
+            ></ViewCatogs>
+
+            <!-- </div> -->
+          </div>
+          <!-- <hr> -->
+        </div>
+      </div>
+    </div>
+    <!-- </div> -->
+    <infinite-loading @infinite="infiniteHandler"></infinite-loading>
+    <!-- <CatogVods v-if="checkTab(2)"/> -->
+    <!-- <TrendingVods v-if="checkTab(3)"/> -->
+    <!-- <Subscription v-if="checkTab(4)"/> -->
   </section>
 </template>
 <script>
-import LatestVod from "@/components/index_videos/latest_vods";
 import ViewCatogs from "@/components/views_catogs";
+import LatestVod from "@/components/index_videos/latest_vods";
+// import ViewCatogs from "@/components/views_catogs";
 import CatogVods from "@/components/index_videos/catog_videos";
 import TrendingVods from "@/components/index_videos/trend_videos";
 import Subscription from "@/components/index_videos/subscription";
@@ -351,7 +470,7 @@ export default {
   components: {
     LatestVod,
     CatogVods,
-
+    ViewCatogs,
     TrendingVods,
     Subscription
   },
@@ -406,6 +525,7 @@ export default {
       }
     });
     return {
+      vid: "",
       videos: [...data.data],
       tab: 1,
       page: 1,
@@ -417,6 +537,36 @@ export default {
   },
 
   methods: {
+     infiniteHandler($state) {
+      axios
+        .post(base_url + "/loadmore", {
+          page: this.page,
+          headers: {
+            "Content-type": "application/x-www-form-urlencoded"
+          }
+        })
+        .then(({ data }) => {
+          // console.log(data.data);
+          this.page += 1;
+          if (data.data.length) {
+            data.data.forEach(element => {
+              let temp = {};
+              temp = element;
+              this.videos.push(temp);
+            });
+            $state.loaded();
+          } else {
+            $state.complete();
+          }
+        });
+    },
+    add_to_watchlist(user_id, v_id) {
+      axios
+        .post(base_url + "/add_to_watchlist/" + user_id + "/" + v_id)
+        .then(res => {
+          this.$toast.success("The Video is added to Watch Later");
+        });
+    },
     setTabs(tab) {
       this.tab = tab;
       this.tab1Style = {};
@@ -520,5 +670,5 @@ export default {
   right: 0rem;
   background-color: #fafafc;
 }
-</style>
+</style>single-channel/114
 
