1b065c663d1d394d791b9cac44c6eeae2f950b21
nashgetch <nashgetch@12gmail.com>
2019-02-11 15:46:34 +0300

    seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,10 +16,10 @@ module.exports = {
     meta: [{
         charset: 'utf-8'
       },
-      // {
-      //   name: 'viewport',
-      //   content: 'width=device-width, initial-scale=1'
-      // }
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      }
     ],
     link: [{
       rel: 'canonical',
