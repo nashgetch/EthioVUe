33066473b23c4ec52d08773e962e424d7103b5f5
nashgetch <nashgetch@12gmail.com>
2019-02-12 16:53:08 +0300

    seo

--- components/index_videos/trend_videos.vue
+++ components/index_videos/trend_videos.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div  class="content-wrapper">
     <div class="mb-2">
       <span class="d-inline-block d-sm-none title">Trending Videos...</span>
       <!-- <div class="container parentDiv"> -->
