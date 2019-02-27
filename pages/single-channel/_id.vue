156a6a7f8008a0dd7e1f5d5a4d88556a939db76e
nashgetch <nashgetch@12gmail.com>
2019-02-27 11:24:20 +0300

    seo

--- pages/single-channel/_id.vue
+++ pages/single-channel/_id.vue
@@ -250,10 +250,10 @@ export default {
   margin-top: -51% !important;
 }
 .nashitiwa {
-  margin-top: -53% !important;
+  margin-top: -44% !important;
 }
 .nashiwwa {
-  margin-top: -32% !important;
+  margin-top: -28% !important;
 }
 .nashitis {
   margin-top: 53% !important;
